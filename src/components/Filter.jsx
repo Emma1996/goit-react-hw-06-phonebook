d31ae1b548254cd8e2b467158139b5bd0df24dab
Emma1996 <mocanpetruta96@gmail.com>
2024-02-04 20:05:56 +0200

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setFilter } from 'components/redux/contactSlice';
 
 const Filter = () => {
